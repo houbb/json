a9fc6f55373817a1de6949db1f7de7c760ed6efe
binbin.hou <1060732496@qq.com>
2019-09-04 19:58:39 +0800

    [Feature] add for new

---
+++ json-core/src/main/java/com/github/houbb/json/core/support/serialize/util/CurrencySerialize.java
@@ -0,0 +1,21 @@
+package com.github.houbb.json.core.support.serialize.util;
+
+import com.github.houbb.heaven.annotation.ThreadSafe;
+import com.github.houbb.json.api.ISerialize;
+
+import java.util.Currency;
+
+/**
+ * 货币 序列化
+ * @author binbin.hou
+ * @since 0.0.2
+ */
+@ThreadSafe
+public class CurrencySerialize implements ISerialize<Currency> {
+
+    @Override
+    public String serialize(Currency currency) {
+        return currency.toString();
+    }
+
+}
