9a8f263c153a6c73d9e7e57f34b3ad5faa925de5
houbb <1060732496@qq.com>
2019-09-09 22:15:19 +0800

    modify branch  TO 0.0.6-SNAPSHOT

--- release_rm.sh
+++ release_rm.sh
@@ -2,9 +2,9 @@
 echo "============================= RELEASE START..."
 
 ## 版本号信息(需要手动指定)
-oldVersion="1.0.0"
-newVersion="1.0.0"
-projectName="csv"
+oldVersion="0.0.6"
+newVersion="0.0.6"
+projectName="json"
 
 # 删除分支
 oldBranchName="release_"${oldVersion}
